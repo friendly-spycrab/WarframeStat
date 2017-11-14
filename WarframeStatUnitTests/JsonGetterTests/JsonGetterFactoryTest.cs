d3f07c5f7d4e4fbd36447a394d170507be6c88f1
friendly-spycrab <fido1999@live.dk>
2017-11-14 11:25:37 +0100

    +Added unit test for GetJsonGetter

---
+++ WarframeStatUnitTests/JsonGetterTests/JsonGetterFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarframeStat.JsonGetter;
+
+namespace WarframeStatUnitTests
+{
+    [TestClass]
+    public class JsonGetterFactoryTest
+    {
+        [TestMethod]
+        public void GetJsonGetterTest()
+        {
+            JsonGetterFactory factory = new JsonGetterFactory();
+            AbstractJsonGetter Jsongetter = factory.GetJsonGetter(JsonGetterType.FromWarframeStat);
+
+            Assert.IsInstanceOfType(Jsongetter,typeof(WarframeStatJsonGetter));
+        }
+    }
+}
