af252645c83698bdfc9703073b442a83833f78ed
friendly-spycrab <fido1999@live.dk>
2017-11-14 11:35:14 +0100

    +Added unit tests for WarframeStatJsonGetter

---
+++ WarframeStatUnitTests/JsonGetterTests/WarframeStatJsonGetterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarframeStat.JsonGetter;
+using WarframeStat.Statistics;
+
+namespace WarframeStatUnitTests.JsonGetterTests
+{
+    [TestClass]
+    public class WarframeStatJsonGetterTest
+    {
+        [TestMethod]
+        public void GetMainStatTest()
+        {
+            WarframeStatJsonGetter jsonGetter = new WarframeStatJsonGetter();
+            MainStat stat = jsonGetter.GetMainStat();
+
+            Assert.IsNotNull(stat);
+        }
+    }
+}
